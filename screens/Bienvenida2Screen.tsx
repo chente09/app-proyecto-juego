89865f957b731f7b28e9fabd760d8152eec06576
chente09 <chentenengerc@gmail.com>
2024-07-03 18:33:39 -0500

    screens primera parte

---
+++ screens/Bienvenida2Screen.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Bienvenida2Screen() {
+  return (
+    <View>
+      <Text>Bienvenida2Screen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
