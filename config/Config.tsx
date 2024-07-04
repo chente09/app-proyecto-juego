516e9bd2c83cc2e03d5fbfa3badde8865a447f6b
NBarchi <nicobarrera.g10@gmail.com>
2024-07-03 19:43:11 -0500

    firebase config

---
+++ config/Config.tsx
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAy0CKJUx4T99RxbZpIK3g0yWNefpegc_g",
+  authDomain: "app-mov-taller-1.firebaseapp.com",
+  databaseURL: "https://app-mov-taller-1-default-rtdb.firebaseio.com",
+  projectId: "app-mov-taller-1",
+  storageBucket: "app-mov-taller-1.appspot.com",
+  messagingSenderId: "328634721579",
+  appId: "1:328634721579:web:99699c7cadfd6a05cb916e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
